c328a6a43ab1d5033ec83c7f552a0ef4cf311af2
albert patterson <apatterson189@gmail.com>
2021-02-26 16:30:28 -0500

    indicate in the store that dates are serializable

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,28 @@
-import { configureStore } from '@reduxjs/toolkit';
+import {
+  configureStore,
+  isPlain,
+  createSerializableStateInvariantMiddleware,
+} from '@reduxjs/toolkit';
 import backlogReducer from './backlogSlice';
 import scheduleReducer from './scheduleSlice';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const isNonPlainSerializable = (value: any) => value instanceof Date;
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const isSerializable = (value: any) =>
+  isPlain(value) || isNonPlainSerializable(value);
+
+const serializableMiddleware = createSerializableStateInvariantMiddleware({
+  isSerializable,
+});
 
 const store = configureStore({
   reducer: {
     backlog: backlogReducer,
     schedule: scheduleReducer,
   },
+  middleware: [serializableMiddleware],
 });
 
 export type RootState = ReturnType<typeof store.getState>;
