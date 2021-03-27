2d84ae0f499605ec48f38999b69686a451766545
albert patterson <apatterson189@gmail.com>
2021-03-26 21:17:42 -0400

    update flow to add backlog to schedule

--- src/store/dispatch_async_thunk.ts
+++ src/store/dispatch_async_thunk.ts
@@ -1,10 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { ThunkDispatch } from 'redux-thunk';
 import { AsyncThunk } from '@reduxjs/toolkit';
 import { Action } from 'redux';
 
 export function dispatchAsyncThunk<D, R, S, E, A extends Action>(
   dispatch: ThunkDispatch<S, E, A>,
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   asyncThunk: AsyncThunk<R, D, any>,
   data: D
 ): Promise<any> {
